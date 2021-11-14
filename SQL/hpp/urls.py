28d945a325ac7b99ff8ee0b518855659156f1dba
anuragdevon <msm19b021@iiitdm.ac.in>
2021-11-14 11:52:21 +0530

    update url routing

--- SQL/hpp/urls.py
+++ SQL/hpp/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('name/search/<str:username>/', views.user_get_money),
-    path('insecure/login/addmoney/username=<str:username>/password=<str:password>/amount=<int:amount>/', views.user_add_money_secured),
+    path('name/<str:username>/', views.user_get_money_test),
+    path('insecure/login/addmoney/username=<str:username>/password=<str:password>/amount=<int:amount>/', views.user_add_money_unsafe),
+    path('insecure/login/transfer/username=<str:username>/amount=<int:amount>/receiver=<str:receiver>/', views.user_transfer_money_unsafe),
     path('secure/login/token/username=<str:username>/password=<str:password>/amount=<int:amount>/', views.get_token),
-    path('secure/login/transfer/username=<str:username>/amount=<int:amount>/receiver=<str:receiver>/token=<str:token>/', views.user_transfer_money),
+    path('secure/login/add/username=<str:username>/password=<str:password>/amount=<int:amount>/token=<str:token>/', views.user_add_money_safe),
+    path('secure/login/transfer/username=<str:username>/amount=<int:amount>/receiver=<str:receiver>/token=<str:token>/', views.user_transfer_money_safe),
 ]
 
 
